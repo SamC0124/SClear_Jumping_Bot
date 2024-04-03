8ce8933f9910dd76b43ed0e04768fc33b822cf6e
Sam C <97773905+SamC0124@users.noreply.github.com>
2024-04-03 16:43:48 -0400

    Add files via upload

---
+++ CONSTANTS.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+amplitude = np.pi/4
+frequency = 8
+phaseOffset = -np.pi/6
+iterations = 1000
+standardMotorForce = 40
+numberOfGenerations = 10
+populationSize = 10
+numSensorNeurons = 9
+numMotorNeurons = 8
+motorAngleRange = 0.6
